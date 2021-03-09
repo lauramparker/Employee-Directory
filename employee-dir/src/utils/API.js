555192f9bea2683d270b493bd451ec317bb55b12
Laura Parker <lauramparker78@gmail.com>
2021-03-08 21:48:00 -0500

    Reducing from 3 to 1 API for MVP

--- employee-dir/src/utils/API.js
+++ employee-dir/src/utils/API.js
@@ -1,29 +1,11 @@
 import axios from "axios";
-import FilterNats from "./FilterNats";
-import FilterName from "./FilterName";
 
 
-// const nats = state.filterNatsValue
+const query = "https://randomuser.me/api/?results=50?inc=name,location&noinfo" //limited to 50
 
-// Random User call for names and locations only (to test) with no seed, results, page, etc
 export default {
-
-  // const nats = "gb"  //testing... should equal drop-down menu selection
-  // const name = "Brad"
-
-
-  getEmployees: function() {
-    return axios.get("https://randomuser.me/api/?inc=name,location&noinfo");
-  },
-
-  filterEmployeeNats: function(nats) {
-    return axios.get("https://randomuser.me/api/?nat=gb")
-  },
-
-  filterEmployeeName: function(name) {
-    return axios.get("https://randomuser.me/api/?name=Brad")
+  getEmployeesList: function(query) {
+    return axios.get(query); 
   }
 
-//Add additional methods for filter & single employee?
-
-};
+}
