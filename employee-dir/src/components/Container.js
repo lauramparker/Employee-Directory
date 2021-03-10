97cf0b0c5267d86875bc7ae5556b63e179b63578
Laura Parker <lauramparker78@gmail.com>
2021-03-09 21:39:28 -0500

    Edit title

--- employee-dir/src/components/Container.js
+++ employee-dir/src/components/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 function Container(props) { 
-    return <div className={`container${props.fluid ? "-fluid" : ""}`}>     <h3>container</h3>
+    return <div className={`container${props.fluid ? "-fluid" : ""}`}> 
         {props.children}</div>;
 
 }
